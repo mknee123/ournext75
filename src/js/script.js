8cc0475f19476b5dbadb6e224dddb2c9798dfbfb
MurKat <mknee123@gmail.com>
2019-03-26 16:37:19 -0400

    Add jQuery for scroll effect on nav links. Add mobile nav styles. Add script for toggle mobile nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,17 @@
 console.log("hello world");
 
+// $(document).ready(function() {
+//     $('#nav-control').on('click', function() {
+//       $('#nav-control').toggleClass('open');
+//       $('#site-body').toggleClass('mainClose');
+//       $('#nav').toggleClass('navClose');
+//     });
+//   });
+
 $(document).ready(function() {
-    
-
-
-    $('#nav-control').on('click', function() {
-      $('#nav-control').toggleClass('open');
-      $('#site-body').toggleClass('mainClose');
-      $('#nav').toggleClass('navClose');
+    $('.toggle-button').on('click', function() {
+      $('.mobile-nav-container').toggleClass('open');
+      $('.toggle-button').toggleClass('close');
+      //$('#nav').toggleClass('navClose');
     });
   });
