03a0601e93ac9df0cd29d5de8076105c938289f0
MurKat <mknee123@gmail.com>
2019-03-26 14:39:05 -0400

    Add site content the html page. Add styles to fonts and body layout. Add scroll effect for links

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+$(document).ready(function() {
+    
+
+
+    $('#nav-control').on('click', function() {
+      $('#nav-control').toggleClass('open');
+      $('#site-body').toggleClass('mainClose');
+      $('#nav').toggleClass('navClose');
+    });
+  });
