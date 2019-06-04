72b8c7458fa804f46d10e44536b438b41987b645
MurKat <mknee123@gmail.com>
2019-06-04 10:28:20 -0400

    Add styles and content to module popup. disabled the cookies for testing

--- src/js/script.js
+++ src/js/script.js
@@ -19,44 +19,47 @@ $('.toggle-button').click(function() {
   }
 });
 
-// var lastFocus;
+var lastFocus;
 
-// function onPopupOpen() {
-//   $("#module").show();
-// }
+function onPopupOpen() {
+  $("#module").show();
+}
 
-// function onPopupClose() {
-//   $("#module").hide();
-//   Cookies.set('colorboxShown', 'yes', {
-//     expires: 1
-//   });
-//   $(".clear-cookie").fadeIn();
-//   lastFocus.focus();
-// }
+function onPopupClose() {
+  $("#module").hide();
+  // Cookies.set('colorboxShown', 'yes', {
+  //   expires: 1
+  // });
+  // $(".clear-cookie").fadeIn();
+  lastFocus.focus();
+}
 
 
-// function displayPopup() {
-//   $.colorbox({
-//     inline: true,
-//     fixed: true,
-//     href: "#module",
-//     className: "cta",
-//     width: 600,
-//     height: 350,
-//     onComplete: onPopupOpen,
-//     onClosed: onPopupClose
-//   });
-// }
-// setTimeout(function(){
-//   var popupShown = Cookies.get('colorboxShown');
+function displayPopup() {
+  $.colorbox({
+    inline: true,
+    fixed: true,
+    href: "#module",
+    className: "cta",
+    width: "100%",
+    maxWidth: 600,
+    height: "100%",
+    maxHeight: 450,
+    onComplete: onPopupOpen,
+    onClosed: onPopupClose
+  });
+}
+setTimeout(function(){
+  // var popupShown = Cookies.get('colorboxShown');
 
-//   if(popupShown){
-//     console.log("Cookie found. No action necessary");
-//   } else {
-//     lastFocus = document.activeElement;
-//     displayPopup();
-//   }
-// }, 2000);
+  // if(popupShown){
+  //   console.log("Cookie found. No action necessary");
+  // } else {
+  //   lastFocus = document.activeElement;
+  //   displayPopup();
+  // }
+  displayPopup()
+}, 2000);
 
 
 
